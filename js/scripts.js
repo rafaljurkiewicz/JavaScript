45dabc70e9ba23ee664ea74dad17348b873f937a
Rafal Jurkiewicz <rafal.jurkiewicz87@gmail.com>
2017-10-29 22:41:24 +0100

    new function from task 8.5 added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // scripts.js file
 
-var name = prompt('Enter your name');
-// abc
-
-/*
-alert('Hello, ' + name);
-*/
-console.log('Hello, ' + name);
+var name = prompt('Enter your name'),
+	a = prompt('Enter base of triangle'),
+	h = prompt('Enter height of triangle'),
+	triangleArea = a*h/2;
+alert('Hello, ' + name + ' Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea),
+console.log('Hello, ' + name + ' Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
