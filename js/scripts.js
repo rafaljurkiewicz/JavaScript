e51fe33efaac1a2a77678f660cf630932dfacb97
Rafal Jurkiewicz <rafal.jurkiewicz87@gmail.com>
2017-11-02 23:15:43 +0100

    task 9.2 - tables

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ if (value > 0) {
 */
 
 
-
+/*
 function getTriangleArea (a, h) {
 	if ( (a <= 0) || (h <= 0) ) {
 		console.log('Wrong data');
@@ -27,3 +27,20 @@ console.log(getTriangleArea(10, 6))
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(10, 20);
 var triangle3Area = getTriangleArea(10, 25);
+*/
+
+
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+if ( allNames.indexOf(newName) === -1 ) {
+	var x = allNames.push(newName);
+}
+
+console.log(x);
+console.log(allNames);
