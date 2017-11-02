39ede6f25b77625181930487549a47e56512c1c8
Rafal Jurkiewicz <rafal.jurkiewicz87@gmail.com>
2017-11-02 22:14:53 +0100

    task 9.1 - function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // scripts.js file
 
+/*
 var	a = prompt('Enter a value'),
 	b = prompt('Enter b value'),
 	value = (a * a) - (2 * a * b) + (b * b);
@@ -10,6 +11,19 @@ if (value > 0) {
 } else {
 	console.log('Outcome is below zero');
 }
+*/
 
 
 
+function getTriangleArea (a, h) {
+	if ( (a <= 0) || (h <= 0) ) {
+		console.log('Wrong data');
+	}
+	return a*h/2
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(10, 20);
+var triangle3Area = getTriangleArea(10, 25);
